4e241458f107b11ae20eab35dc0be4613876b581
Shekhar Dwivedi <105995198+shekhar-dwivedi@users.noreply.github.com>
2023-07-08 16:38:42 +0530

    Add files via upload

--- slider.js
+++ slider.js
@@ -14,17 +14,17 @@ navToggle.addEventListener("click", function () {
   }
 });
 // ********Carousel***********
-const sliders = document.querySelectorAll('.slider');
+const sliders = document.querySelectorAll(".slider");
 
 sliders.forEach((slider) => {
   const sliderWrapper = slider.parentNode;
-  const sliderCards = slider.querySelectorAll('.slider-card');
-  const prevButton = sliderWrapper.querySelector('.slider-control.prev');
-  const nextButton = sliderWrapper.querySelector('.slider-control.next');
+  const sliderCards = slider.querySelectorAll(".slider-card");
+  const prevButton = sliderWrapper.querySelector(".slider-control.prev");
+  const nextButton = sliderWrapper.querySelector(".slider-control.next");
   const cardWidth = sliderCards[0].offsetWidth;
   let currentPosition = 0;
 
-  nextButton.addEventListener('click', () => {
+  nextButton.addEventListener("click", () => {
     currentPosition -= cardWidth;
     if (currentPosition < -((sliderCards.length - 3) * cardWidth)) {
       currentPosition = -((sliderCards.length - 3) * cardWidth);
@@ -32,11 +32,64 @@ sliders.forEach((slider) => {
     slider.style.transform = `translateX(${currentPosition}px)`;
   });
 
-  prevButton.addEventListener('click', () => {
+  prevButton.addEventListener("click", () => {
     currentPosition += cardWidth;
     if (currentPosition > 0) {
       currentPosition = 0;
     }
     slider.style.transform = `translateX(${currentPosition}px)`;
+  });
+});
+
+//slider
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollContainers = document.querySelectorAll(".scroll");
+  const leftButtons = document.querySelectorAll(".left-btn");
+  const rightButtons = document.querySelectorAll(".right-btn");
+
+  scrollContainers.forEach((scrollContainer, index) => {
+    let isDown = false;
+    let scrollX;
+    let scrollLeft;
+
+    // Mouse Up Function
+    scrollContainer.addEventListener("mouseup", () => {
+      isDown = false;
+      scrollContainer.classList.remove("active");
+    });
+
+    // Mouse Leave Function
+    scrollContainer.addEventListener("mouseleave", () => {
+      isDown = false;
+      scrollContainer.classList.remove("active");
+    });
+
+    // Mouse Down Function
+    scrollContainer.addEventListener("mousedown", (e) => {
+      e.preventDefault();
+      isDown = true;
+      scrollContainer.classList.add("active");
+      scrollX = e.pageX - scrollContainer.offsetLeft;
+      scrollLeft = scrollContainer.scrollLeft;
+    });
+
+    // Mouse Move Function
+    scrollContainer.addEventListener("mousemove", (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const element = e.pageX - scrollContainer.offsetLeft;
+      const scrolling = (element - scrollX) * 2;
+      scrollContainer.scrollLeft = scrollLeft - scrolling;
+    });
+
+    // Left Button Click Function
+    leftButtons[index].addEventListener("click", () => {
+      scrollContainers[index].scrollLeft -= 500; // Adjust the scrolling amount as needed
+    });
+
+    // Right Button Click Function
+    rightButtons[index].addEventListener("click", () => {
+      scrollContainers[index].scrollLeft += 500; // Adjust the scrolling amount as needed
+    });
   });
 });
